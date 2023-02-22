112e8f2778925bbe25772e8efdcf619c9b9e51fd
Agnes Kaswii <agneskaswii700@gmail.com>
2023-02-22 11:47:11 +0300

    adding project

---
+++ project/urls.py
@@ -0,0 +1,24 @@
+
+from django.contrib import admin
+from django.urls import include,path
+from rest_framework import routers
+from members import views
+
+router = routers.SimpleRouter()
+
+router.register(r'members', views.MembersViewSet)
+router.register(r'accounts', views.AccountsViewSet)
+router.register(r'cards', views.CardsViewSet)
+
+
+
+urlpatterns = [
+  
+    path('admin/', admin.site.urls),
+    path('' ,include(router.urls)),
+    # path('', include('members.urls')),
+    # path('', include('swagger_ui.urls')),
+
+ 
+   
+]
